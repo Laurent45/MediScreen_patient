f204e0c77e726bb34f862bb0d707cfc502472b10
laurentfrederick <laurent.frederick@icloud.com>
2022-03-18 17:40:53 +0100

    init commit

---
+++ src/main/java/com/openclassrooms/mediscreen/MediScreenApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.mediscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediScreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediScreenApplication.class, args);
+    }
+
+}
